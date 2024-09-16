e31e92056d641db4e1694367061a111a767f2920
Joe Toledano <joseph.a.toledano@gmail.com>
2024-09-16 09:22:35 -0700

    fixing typing issue with linkedin package

--- src/handlers/jobs/index.ts
+++ src/handlers/jobs/index.ts
@@ -2,6 +2,7 @@ import { getJobsFromLinkedin } from "../../services/jobs";
 
 export async function getJobs(c: any) {
 	// const { email, preferences } = await c.req.json();
+
 	const result = await getJobsFromLinkedin({});
 	return c.json(result);
 }
