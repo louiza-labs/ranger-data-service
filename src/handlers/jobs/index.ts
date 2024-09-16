731d08c723b81c554d1603b519f56b2c660ded00
Joe Toledano <joseph.a.toledano@gmail.com>
2024-09-16 11:29:20 -0700

    added in job uploads of linkedin job endpoint to db

--- src/handlers/jobs/index.ts
+++ src/handlers/jobs/index.ts
@@ -1,8 +1,35 @@
-import { getJobsFromLinkedin } from "../../services/jobs";
+import { getJobsFromLinkedin, getJobsFromLinkedinFromDB, uploadJobsFromLinkedInToDB } from "../../services/jobs";
 
 export async function getJobs(c: any) {
-	// const { email, preferences } = await c.req.json();
+	const { page, keyword, location, dateSincePosted, limit } = await c.req.query();
 
-	const result = await getJobsFromLinkedin({});
-	return c.json(result);
+	const { data: jobsFetchedFromLinkedin } = await getJobsFromLinkedin({
+		page,
+		keyword,
+		location,
+		dateSincePosted,
+		limit,
+	});
+
+	const { data: jobsFromDB } = await getJobsFromLinkedinFromDB();
+
+	if (jobsFromDB?.length && jobsFetchedFromLinkedin?.length) {
+		// Filter jobs that are not already in the DB
+		const filteredJobsFromLinkedin = jobsFetchedFromLinkedin.filter(
+			(job: any) => !jobsFromDB.find((jobFromDB: any) => job.jobUrl === jobFromDB.jobUrl)
+		);
+
+		if (filteredJobsFromLinkedin.length) {
+			// Upload new jobs
+			const res = await uploadJobsFromLinkedInToDB(filteredJobsFromLinkedin);
+			return c.json([...filteredJobsFromLinkedin, jobsFromDB]);
+		}
+	} else if (jobsFetchedFromLinkedin?.length) {
+		// No jobs in DB, upload all fetched jobs
+		const res = await uploadJobsFromLinkedInToDB(jobsFetchedFromLinkedin);
+		return c.json(jobsFetchedFromLinkedin);
+	}
+
+	// If no jobs were fetched or uploaded, return an empty response
+	return c.json({ message: "No new jobs to upload" });
 }
--- src/services/jobs/index.ts
+++ src/services/jobs/index.ts
@@ -6,30 +6,97 @@ interface taglineObject {
 	hash: string;
 }
 
+interface JobListing {
+	position: string;
+	company: string;
+	companyLogo: string;
+	location: string;
+	date: string;
+	agoTime: string;
+	salary: string;
+	jobUrl: string;
+}
+
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl as string, supabaseAnonKey as string);
 
-export async function getJobsFromLinkedin({}: any) {
+interface jobsFromLinkedinArgs {
+	page?: number;
+	keyword?: string;
+	location?: string;
+	dateSincePosted?: "past month" | "past week" | "24hr";
+	limit?: string;
+}
+export async function getJobsFromLinkedin({
+	page = 0,
+	keyword = "software engineer",
+	location = "New York, NY",
+	dateSincePosted = "past week",
+	limit = "10",
+}: jobsFromLinkedinArgs) {
 	try {
 		const queryOptions = {
-			keyword: "software engineer",
-			location: "New York",
-			dateSincePosted: "past Week",
+			keyword: String(keyword),
+			location: String(location),
+			dateSincePosted: String(dateSincePosted),
 			jobType: "full time",
 			// remoteFilter: "remote",
 			// salary: "100000",
 			// experienceLevel: "entry level",
-			limit: "10",
-			page: "1",
+			limit: String(limit) ?? "10",
+			page: String(page),
 		};
-
-		const res = linkedIn.query(queryOptions).then((response: any) => {
-			return response;
-		});
-		return res;
+		const res = await linkedIn.query(queryOptions);
+		const urlRes = await fetch(
+			"https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=software&location=New+York,%20NY&f_TPR=r604800&f_JT=F&start=50"
+		);
+		return { data: res, count: res && res.length ? res.length : 0 };
 	} catch (e) {
 		console.error("Error fetching jobs:", e);
-		return { success: false, error: e, data: [] };
+		return { success: false, error: e, data: [], count: 0 };
+	}
+}
+
+export async function getJobsFromLinkedinFromDB() {
+	try {
+		const { data, error } = await supabase
+			.from("linkedin-jobs") // Replace with your table name
+			.select("*");
+
+		if (error) {
+			console.error("Error getting jobs:", error);
+			return { success: false, error };
+		}
+
+		console.log("Got jobs:", data);
+		return { success: true, data };
+	} catch (e) {
+		return { success: false, e };
+	}
+}
+
+export async function uploadJobsFromLinkedInToDB(jobListings: JobListing[]) {
+	try {
+		const formattedJobListings = jobListings.map((jobListing: JobListing) => ({
+			...jobListing,
+			job_id: `${jobListing.position}-${jobListing.company}-${jobListing.location}-${jobListing.date}`,
+		}));
+
+		const { data, error } = await supabase
+			.from("linkedin-jobs") // Replace with your table name
+			.upsert(formattedJobListings, {
+				onConflict: "job_id",
+			});
+
+		if (error) {
+			console.error("Error upserting jobs:", error);
+			return { success: false, error };
+		}
+
+		console.log("Upserted jobs:", data);
+		return { success: true, data };
+	} catch (e) {
+		return { success: false, e };
 	}
 }
