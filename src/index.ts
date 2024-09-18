c922458b5539a9a12d9312d2c0325d9bb9ef5991
Joe Toledano <joseph.a.toledano@gmail.com>
2024-09-17 19:50:49 -0700

    added in bearer token auth

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 
+import { bearerAuth } from "hono/bearer-auth";
+
 import accountRoute from "./routes/account";
 import connectionsRoute from "./routes/connections";
 import emailsRoute from "./routes/email";
@@ -9,6 +11,7 @@ import subscriptionRoute from "./routes/subscribe";
 
 const app = new Hono();
 app.use(logger());
+app.use(bearerAuth({ token: process.env.VALID_API_KEY ?? "😭" }));
 
 // Use the routes
 app.route("/api", subscriptionRoute);
