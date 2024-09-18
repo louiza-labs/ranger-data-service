05de4a3144c1ed20b6bce71621539b355502fe9e
Joe Toledano <joseph.a.toledano@gmail.com>
2024-09-17 20:56:40 -0700

    adding and fixing the account routes

--- src/routes/account/index.ts
+++ src/routes/account/index.ts
@@ -1,10 +1,14 @@
 // src/routes/alertsRoute.ts
 import { Hono } from "hono";
+import createAccountRoutes from "./create";
 import preferencesRoutes from "./preferences";
+import verifyAccountRoutes from "./verify";
 const accountRoute = new Hono();
 
 accountRoute.route("/account/preferences", preferencesRoutes);
 // for new accounts
-accountRoute.route("/account/create", preferencesRoutes);
+
+accountRoute.route("/account/create", createAccountRoutes);
+accountRoute.route("/account/verify", verifyAccountRoutes);
 
 export default accountRoute;
